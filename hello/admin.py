d0624e0f606ea5ebc6bfc6beb1f48c4baf43ee56
nitingera1996 <nitin.gera.ece14@iitbhu.ac.in>
2015-09-12 17:10:48 +0530

    models updated

--- hello/admin.py
+++ hello/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from hello.models import User
+from hello.models import User,Event,Parent_Event,Post,Team
 admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Team)
+admin.site.register(Parent_Event)
+admin.site.register(Event)
 
 # Register your models here.
--- hello/models.py
+++ hello/models.py
@@ -1,17 +1,54 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
 class User(models.Model):
-	name = models.CharField(max_length=100)
-	phone = models.CharField(max_length=20)
-	email = models.EmailField(max_length=100)
-	college = models.CharField(max_length=100)
-	year = models.CharField(max_length=5)
-	city = models.CharField(max_length=30)
-	password = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    password = models.CharField(max_length=200)
+    college = models.CharField(max_length=100)
+    year = models.CharField(max_length=5)
+    points=models.IntegerField(default=0)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
+
+class Parent_Event(models.Model):
+    name=models.CharField(max_length=100)
+    order=models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    name=models.CharField(max_length=100)
+    content=models.TextField()
+    parent_event=models.ForeignKey(Parent_Event)
+    order=models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=100)
+    #team_leader=models.ForeignKey(User)
+    team_members=models.ManyToManyField(User)
+    event=models.ManyToManyField(Event)
+
+    def __str__(self):
+        return self.team_name
+
+
+class Post(models.Model):
+    title=models.CharField(max_length=200)
+    content=models.TextField()
+    event=models.ForeignKey(Event)
+    datetime_added=models.DateTimeField(default=datetime.now())
+    priority=models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title

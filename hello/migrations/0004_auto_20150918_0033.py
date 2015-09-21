1477fcbda7adf9efb0b3eb1f522e06ba1ca3b08b
nitingera1996 <nitin.gera.ece14@iitbhu.ac.in>
2015-09-21 22:28:07 +0530

    team modfication updated

---
+++ hello/migrations/0004_auto_20150918_0033.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hello', '0003_auto_20150917_2340'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='datetime_added',
+            field=models.DateTimeField(default=datetime.datetime(2015, 9, 18, 0, 33, 34, 384371)),
+        ),
+    ]

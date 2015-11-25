2cfc3c2360386aef04112bb575a4a84714ed9a65
Omkar Jadhav <omkarjadhav003@gmail.com>
2015-11-26 01:19:41 +0530

    requests added

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
